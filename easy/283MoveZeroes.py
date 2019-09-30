e6a8b304d1cf4808014695fd5f2abc6e42cc77fe
vincentx_3 <2440027575@qq.com>
2019-09-30 19:31:31 +0800

    easy:283 列表 in-place操作

---
+++ easy/283MoveZeroes.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeros, real_index = 0, 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                zeros += 1
+            else:
+                nums[real_index] = nums[i]
+                real_index += 1
+        nums[real_index:] = [0 for x in range(zeros)]
+
+        return
